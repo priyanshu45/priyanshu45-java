049a5536f4bb43c7bf829659167d2989f021d1a1
priyanshu chaurasia <chaurasiapriyanshu4@gmail.com>
2021-01-10 08:03:23 +0000

    n=10

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@
 // 2i+1 = Odd Numbers = 1, 3, 5, 7, 9
 class Main {
   public static void main(String[] args) {
-    int n = 100000;
+    int n = 10;
     for (int i = 0; i < n; i++) {
         System.out.println(2*i+n);
     }
